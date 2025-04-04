c886008b2ef27ac91f695cfe04d662e294fea290
dennis0822 <dennis.940822@gmail.com>
2025-04-05 00:11:25 +0800

    login2

--- EstateAgency/LogIn.php
+++ EstateAgency/LogIn.php
@@ -11,13 +11,12 @@
           session_start();
           $u_email = $_POST['u_email'];
           $u_password = $_POST['u_password'];
-          $link = mysqli_connect('localhost', 'root', '', 'IndustrialPlatform');
-          $sql = "select * from corporation_account where u_email='$u_email' and u_password ='$u_password'";
+          $link = mysqli_connect('localhost', 'root', '', 'sa');
+          $sql = "select * from user_account where u_email='$u_email' and u_password ='$u_password'";
           $rs = mysqli_query($link, $sql);
           if ($record = mysqli_fetch_assoc($rs))
           {
             $_SESSION['u_email'] = $record['u_email'];
-            $_SESSION['c_name'] = $record['c_name'];
             $_SESSION['u_content'] = $record['u_content'];
             header('location:index.php?method=message&message=登入成功');
           }
