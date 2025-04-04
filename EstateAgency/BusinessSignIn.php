acec8d24ebab920063b54e0b7c26819719aee631
ron <ron@lamsamyick.com>
2025-04-04 21:48:01 +0800

    php

--- EstateAgency/BusinessSignIn.php
+++ EstateAgency/BusinessSignIn.php
@@ -260,3 +260,35 @@
 </body>
 
 </html>
+
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "12345678";
+$dbname = "SA";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Sanitize and validate inputs
+  $c_name=$_POST['c_name'];
+  $c_type=$_POST['c_type'];
+  $c_industry=$_POST['c_industry'];
+  $c_address =$_POST['c_address'];
+  $c_email =$_POST['c_email'];
+  $c_phone=$_POST['c_phone'];
+
+  $e_name=$_POST['e_name'];
+  $e_type=$_POST['e_type'];
+  $e_email=$_POST['e_email'];
+  $e_phone=$_POST['e_phone'];
+
+  $u_email=$_POST['u_email'];
+  $u_password=$_POST['u_password'];
+  $u_content=$_POST['u_content'];
+}
+$sql = "INSERT INTO corporation_account (c_name, c_type, c_industry, c_address, c_email, c_phone, e_name, e_type, e_email, e_phone, u_email, u_password, u_content)
+  VALUES ('$c_name', '$c_type', '$c_industry', '$c_address', '$c_email', '$c_phone', '$e_name', '$e_type', '$e_email', '$e_phone', '$u_email', '$u_password', '$u_content')";
+$conn->close()
+?>
