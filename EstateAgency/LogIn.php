09b7670cec215cd117a89bf10e9deb93c4b77d31
張文謙 <chesteras452@gmail.com>
2025-04-05 23:55:23 +0800

    首頁

--- EstateAgency/LogIn.php
+++ EstateAgency/LogIn.php
@@ -11,18 +11,18 @@
           session_start();
           $u_email = $_POST['u_email'];
           $u_password = $_POST['u_password'];
-          $link = mysqli_connect('localhost', 'root', '', 'sa');
+          $link = mysqli_connect('localhost', 'root', '12345678', 'sa');
           $sql = "select * from user_account where u_email='$u_email' and u_password ='$u_password'";
           $rs = mysqli_query($link, $sql);
           if ($record = mysqli_fetch_assoc($rs))
           {
             $_SESSION['u_email'] = $record['u_email'];
             $_SESSION['u_content'] = $record['u_content'];
-            header('location:index.php?method=message&message=登入成功');
+            header('location:index.html?method=message&message=登入成功');
           }
           else
           {
-            header('location:index.php?method=message&message=登入失敗');
+            header('location:index.html?method=message&message=登入失敗');
           }
         ?>
     </p>
