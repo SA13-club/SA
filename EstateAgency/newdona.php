95f47632ee2f17ebb37ecdb23026afd7c0da715c
ron <ron@lamsamyick.com>
2025-04-26 21:58:47 +0800

    有bug

--- EstateAgency/newdona.php
+++ EstateAgency/newdona.php
@@ -112,15 +112,17 @@
                                 <textarea class="form-control" name="content" rows="3" placeholder="請詳細說明需求和期望"
                                     required></textarea>
                             </div> -->
+<?php
+$u_permission=$_SESSION['u_permission'];
 
+if($u_permission=='組織團體'){
+echo'
                             <p>一、請選擇需求類型</p>
             <div class="col-md-6">
             <select class="form-select" id="demandtype" name="tag" required>
                 <option selected disabled value="_">需求類型</option>
                 <option value="spon">贊助</option>
-                <option value="合作">合作</option>
-                <option value="招募">招募</option>
-                <option value="實習">實習</option>
+                
             </select>
             </div>
 
@@ -208,18 +210,140 @@
     </div>
   </div>
 </div>
+';}
+elseif ($u_permission == '企業') {
+    echo '
+    <p>一、請選擇需求類型</p>
+    <div class="col-md-6">
+        <select class="form-select" id="demandtype2" name="tag" required>
+            <option selected disabled value="_">需求類型</option>
+            <option value="spon">贊助</option>
+            <option value="合作">合作</option>
+            <option value="招募">招募</option>
+            <option value="實習">實習</option>
+        </select>
+    </div>
+
+    <div class="row gy-4">
+        <!-- 贊助細節區塊 -->
+        <div id="sponsorSection2" style="display: none;">
+            <p>二、贊助細節</p>
+            <div class="row gy-3">
+                <div class="col-md-4">
+                    <select class="form-select" id="sponsor_method2" name="sponsor_method" required>
+                        <option selected disabled value="_">贊助方式</option>
+                        <option value="money">金錢</option>
+                        <option value="product">產品</option>
+                    </select>
+                </div>
+
+                <!-- 金錢贊助細節 -->
+                <div id="sponsor_amount2" style="display: none;">
+                    <div class="col-md-4">
+                        <select class="form-select" id="sponsor_amount_select" name="sponsor_amount" required>
+                            <option selected disabled value="_">贊助金額</option>
+                            <option value="10000">10000以內</option>
+                            <option value="20000">10000~20000</option>
+                            <option value="30000">20000~30000</option>
+                            <option value="40000">30000~40000</option>
+                            <option value="50000">40000~50000</option>
+                            <option value="other">金額詳談</option>
+                        </select>
+                    </div>
+                    <div class="col-md-6" id="money_options2">
+                        <label>需要社團宣傳手段</label>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="postbrand" value="海報商標" id="posterLogo">
+                            <label class="form-check-label" for="posterLogo">海報商標</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="postad" value="海報置入" id="posterInsert">
+                            <label class="form-check-label" for="posterInsert">海報置入</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="promotesignage" value="宣傳立牌" id="standee">
+                            <label class="form-check-label" for="standee">宣傳立牌</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="socialpromote" value="社群宣傳" id="social">
+                            <label class="form-check-label" for="social">社群宣傳</label>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- 產品贊助細節 -->
+                <div id="productdona2" style="display: none;">
+                    <div class="col-md-6" id="product_options">
+                        <label>需要社團宣傳手段</label>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="product[]" value="公關品發放" id="gift">
+                            <label class="form-check-label" for="gift">公關品發放</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="product[]" value="現場設攤位" id="booth">
+                            <label class="form-check-label" for="booth">現場設攤位</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- 實習細節區塊 -->
+        <div id="internsection" style="display: none;">
+            <p>二、實習職缺資訊</p>
+            <div class="row gy-3">
+                <div class="col-md-4">
+                    <label>職缺名稱</label>
+                    <input type="text" class="form-control" name="recruit_title" placeholder="招募職缺名稱">
+                </div>
+                <div class="col-md-4">
+                    <label>人數</label>
+                    <input type="number" class="form-control" name="recruit_number" placeholder="招募人數">
+                </div>
+                <div class="col-md-4">
+                    <label>地點</label>
+                    <select id="city" class="form-select" name="recruit_city">
+                        <option selected disabled value="">選擇縣市</option>
+                        <option value="台北市">台北市</option>
+                        <option value="新北市">新北市</option>
+                        <option value="桃園市">桃園市</option>
+                        <!-- (後續略) -->
+                    </select>
+                </div>
+                <div class="col-md-4">
+                    <select id="district" class="form-select" name="recruit_district">
+                        <option selected disabled value="">選擇行政區</option>
+                    </select>
+                </div>
+
+                <div class="col-md-12">
+                    <textarea class="form-control" name="recruit_detail" rows="3" placeholder="職缺說明與條件"></textarea>
+                </div>
+
+                <div class="col-md-6">
+                    <label>應徵條件</label>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" name="ex" value="具備相關經驗" id="experience">
+                        <label class="form-check-label" for="experience">具備相關經驗</label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" name="language" value="語言能力" id="language">
+                        <label class="form-check-label" for="language">語言能力</label>
+                    </div>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" name="subject" value="相關科系" id="subject">
+                        <label class="form-check-label" for="subject">相關科系</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>';
+}
+?>
 
 
-<!-- 實習細節區塊 -->
-<div id="internSection" style="display: none;">
-  <p>二、實習需求人數</p>
-  <div class="col-md-4">
-    <input type="number" class="form-control" name="intern_people" placeholder="請輸入需求人數">
-  </div>
-</div>
 
-
-                            <p>五、負責人與聯絡資訊</p>
+                            <p>三、負責人與聯絡資訊</p>
 
                             <div class="col-md-5">
                                 <input type="text" class="form-control" name="name" placeholder="主要聯絡人姓名" required="">
@@ -231,7 +355,7 @@
                             <div class="col-md-5">
                                 <input type="tel" class="form-control" name="phone" placeholder="聯絡人手機號碼" required="">
                             </div>
-                            <p>六、需求截止日期</p>
+                            <p>四、需求截止日期</p>
                             <div class="col-md-5">
                                 <input type="date" name="date" class="form-control">
                             </div>
@@ -248,6 +372,31 @@
         </section><!-- /Starter Section Section -->
 
     </main>
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     <footer id="footer" class="footer light-background">
 
@@ -336,21 +485,19 @@
     <script>
   const typeSelect = document.getElementById("demandtype");
   const sponsorSection = document.getElementById("sponsorSection");
-  const internSection = document.getElementById("internSection");
+  
 
   typeSelect.addEventListener("change", function () {
     const value = this.value;
 
     // 隱藏所有區塊
     sponsorSection.style.display = "none";
-    internSection.style.display = "none";
+    
 
     // 顯示對應的區塊
     if (value === "spon") {
       sponsorSection.style.display = "block";
-    } else if (value === "實習") {
-      internSection.style.display = "block";
-    }
+    } 
   });
 
 
@@ -376,12 +523,87 @@
         productdona.style.display = "block";
     }
   });
-</script>
+
+
+  
 
 
 
 
 
+
+
+  
+  const typeSelect2 = document.getElementById("demandtype2");
+  const sponsorSection2 = document.getElementById("sponsorSection2");
+  const internsection = document.getElementById("internsection");
+  
+
+  typeSelect2.addEventListener("change", function () {
+    const value = this.value;
+
+    // 隱藏所有區塊
+    sponsorSection2.style.display = "none";
+    internsection.style.display="none";
+    // 顯示對應的區塊
+    if (value === "spon") {
+        sponsorSection2.style.display = "block";
+    } 
+    else if (value === "實習"){
+        internsection.style.display="block";
+
+    }
+  });
+
+
+
+  const sponsor_method2 = document.getElementById("sponsor_method2");
+  const sponsor_amount2= document.getElementById("sponsor_amount2");
+  const productdona2= document.getElementById("productdona2");
+  
+
+    sponsor_method2.addEventListener("change", function () {
+    const value = this.value;
+
+    // 隱藏所有區塊
+    sponsor_amount2.style.display = "none";
+    productdona2.style.display = "none";
+    
+
+    // 顯示對應的區塊
+    if (value === "money") {
+        sponsor_amount2.style.display = "block";
+    } else if(value=="product")
+    {
+        productdona2.style.display = "block";
+    }
+  });
+//行政區
+                        const districtData = {
+                            "台北市": ["中正區", "大同區", "中山區", "松山區", "大安區", "萬華區", "信義區", "士林區", "北投區", "內湖區", "南港區", "文山區"],
+                            "新北市": ["板橋區", "新莊區", "中和區", "永和區", "土城區", "樹林區", "三重區", "新店區", "蘆洲區", "汐止區", "淡水區", "三峽區", "鶯歌區", "瑞芳區", "五股區", "泰山區", "林口區", "深坑區", "石碇區", "坪林區", "三芝區", "石門區", "八里區", "平溪區", "雙溪區", "貢寮區", "金山區", "萬里區", "烏來區"],
+                            "桃園市": ["桃園區", "中壢區", "平鎮區", "八德區", "楊梅區", "蘆竹區", "大溪區", "龍潭區", "龜山區", "大園區", "觀音區", "新屋區", "復興區"],
+                            "台中市": ["中區", "東區", "南區", "西區", "北區", "北屯區", "西屯區", "南屯區", "太平區", "大里區", "霧峰區", "烏日區", "豐原區", "后里區", "石岡區", "東勢區", "和平區", "新社區", "潭子區", "大雅區", "神岡區", "大肚區", "沙鹿區", "龍井區", "梧棲區", "清水區", "大甲區", "外埔區", "大安區"],
+                            // 可以繼續補齊其他縣市喔
+                        };
+
+                        document.getElementById('city').addEventListener('change', function() {
+                            const city = this.value;
+                            const districtSelect = document.getElementById('district');
+                            
+                            // 清空原本的行政區選項
+                            districtSelect.innerHTML = '<option selected disabled value="">選擇行政區</option>';
+                            
+                            if (districtData[city]) {
+                                districtData[city].forEach(function(district) {
+                                    const option = document.createElement('option');
+                                    option.value = district;
+                                    option.textContent = district;
+                                    districtSelect.appendChild(option);
+                                });
+                            }
+                        });
+                        </script>
 
 
 
